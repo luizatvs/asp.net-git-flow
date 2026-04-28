fb5a80dda16e732d31f7a0b58dbc4a23884ed559
luizatvs <maria.l.santos13@aluno.senai.br>
2026-04-27 22:30:09 -0300

    Pais

---
+++ Atividade/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Atividade.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Atividade.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
