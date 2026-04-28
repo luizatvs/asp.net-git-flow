3d0007a413bca9d0497efb7d475387e39562e274
luizatvs <maria.l.santos13@aluno.senai.br>
2026-04-27 22:39:50 -0300

    Pais

---
+++ Atividade/Controllers/PaisController.cs
@@ -0,0 +1,21 @@
+﻿using Atividade.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Atividade.Controllers
+{
+    public class PaisController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Lista de paises";
+            return View();
+        }
+
+        public IActionResult Detalhes()
+        {
+            ViewData["Title"] = "Detalhes de paises";
+            return View();
+        }
+    }
+}
